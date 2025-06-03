5fa0217dad09005828d24da3c82bc7c116e1fc27
Isa Reinert <reinert.isa@gmail.com>
2025-06-02 23:47:20 -0700

    feat(website): useState hook for song state management

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,11 +1,28 @@
+import {useState} from 'react';
+import { v4 as uuid } from 'uuid';
+
+type SongState = {
+    id: number | string,
+    title: string
+}
 
 export default function SongList() {
+    const [songs, setSongs] = useState<SongState[]>([
+        {id: 1, title: 'Song 1'},
+        {id: 2, title: 'Song 2'},
+        {id: 3, title: 'Song 3'},
+    ]);
+
+    const addSong = (): void => setSongs([...songs, {id: uuid(), title: 'new song'}])
+
     return (
-        <div className="song-lig">
+        <div className="song-list">
             <ul>
-                <li>Song list - 1</li>
-                <li>Song list - 2</li>
+                {songs.map(song => (
+                    <li key={song.id}>{song.title}</li>
+                ))}
             </ul>
+            <button onClick={addSong}>Add a song</button>
         </div>
     )
 }
